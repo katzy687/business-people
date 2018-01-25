1b0a208850b9566326a5787454127d4e4d4a3fca
Natti Katz <katzy687@gmail.com>
2018-01-25 19:53:55 +0200

    part one done

--- app.js
+++ app.js
@@ -4,7 +4,15 @@
 var express = require ('express');
 var app = express();
 
+const data = require('./public/js/mock.json');
+console.log(data);
+
 app.use("/", express.static("./public"));
+
+app.get('/getPersonsData', (req, res) => {
+  res.json(data);
+})
+
 
 app.listen(process.env.PORT || 7000);
 console.log('listening on port 7000');
--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,43 @@
 (function () {
   'use strict';
+  const app = angular
+    .module('zoomInfo', ['ngMaterial'])
+    .directive('errSrc', ErrSrc)
+    .factory('PeopleFactory', PeopleFactory)
+    .controller('PersonDataCtrl', PersonDataCtrl);
 
+  // Factory
+  PeopleFactory.$inject = ['$http'];
+  function PeopleFactory($http) {
+    return $http.get('/getPersonsData').then((res) => {
+      return res.data;
+    });
+  }
 
-  angular
-      .module('zoomInfo', [])
-      .controller('PersonDataCtrl', PersonDataCtrl);
-
-  PersonDataCtrl.$inject = [];
-
-  function PersonDataCtrl() {
+  // Controller
+  PersonDataCtrl.$inject = ['$scope', 'PeopleFactory'];
+  function PersonDataCtrl($scope, PeopleFactory) {
     // please name the mock data array you are getting from the nodeJS route, "peopleData"
+    this.$onInit = () => {
+      PeopleFactory.then((res) => {
+        $scope.peopleData = res;
+        console.log(res);
+      })
+    }
 
   }
 
+  // Directive
+  function ErrSrc() {
+    return {
+      link: function (scope, element, attrs) {
+        element.bind('error', function () {
+          if (attrs.src != attrs.errSrc) {
+            attrs.$set('src', attrs.errSrc);
+          }
+        });
+      }
+    }
+  };
 
 })();
